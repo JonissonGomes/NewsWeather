e09a6dc25d51a3e535d2a2ac57a0537cb6aa4f22
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-05-28 12:38:28 -0300

    Iniciando aplicação de previsao do tempo

---
+++ App.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+/* Importando paginas */
+import Home from './src/Home/Home';
+import Consulta from './src/Consulta/Consulta';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Consulta" component={Consulta} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}

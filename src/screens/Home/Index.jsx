e76f7cafcae74429c13882a369ee898855bbfbd4
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-05-29 22:20:07 -0300

    Estilizando interface inicial

--- src/screens/Home/Index.jsx
+++ src/screens/Home/Index.jsx
@@ -1,12 +1,71 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-     flex: 1,
-     alignItems: 'center',
-     marginLeft: 10,
-     marginRight: 10
-    },
-  });
-  
-  export default styles;
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#c0e4d5e7',
+    flexDirection: "column",
+    padding: 0,
+    margin: 0,
+  },
+
+  background: {
+    width: '100%',
+    height: '100%',
+  },
+
+  title: {
+    width: '100%',
+    fontSize: 62,
+    marginLeft: 125,
+    marginRight: 100,
+    marginTop: 240,
+    color: '#ffed26',
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(114, 107, 0, 0.75)',
+    textShadowOffset: { width: 2, height: 4 },
+    textShadowRadius: 10,
+  },
+
+  titleSub: {
+    width: '100%',
+    fontSize: 62,
+    marginLeft: 100,
+    marginRight: 100,
+    marginBottom: 50,
+    color: '#ffed26',
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(114, 107, 0, 0.75)',
+    textShadowOffset: { width: 2, height: 4 },
+    textShadowRadius: 10,
+  },
+
+  inputText: {
+    backgroundColor: '#fff',
+    height: 40,
+    marginLeft: 70,
+    marginRight: 70,
+    marginBottom: 20,
+    borderRadius: 5,
+    padding: 10,
+  },
+
+  fakeButton: {
+    alignItems: 'center',
+    backgroundColor: '#007bffe7',
+    padding: 8,
+    paddingLeft: 14,
+    paddingRight: 14,
+    borderRadius: 5,
+    marginLeft: 120,
+    marginRight: 120,
+  },
+
+  textButton: {
+    fontSize: 18,
+    color: '#fff',
+  },
+
+});
+
+export default styles;

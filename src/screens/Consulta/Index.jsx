19c2e50cf61559bf811d677e7ccc7828eee8b841
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-05-29 22:21:11 -0300

    Organizando estrutura do componente de estilizaçao

---
+++ src/screens/Consulta/Index.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 40,
+        alignItems: 'center',
+        backgroundColor: '#f122',
+    },
+});
+
+export default styles;

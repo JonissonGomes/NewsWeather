c19864707ba1007cee6018557a4020e16cbb7473
Jonisson-Gomes <jonisson.silva@sistemafiepe.org.br>
2021-06-01 12:06:10 -0300

    Criando funçao de consumo da API e resoluçao de bugs com teclado

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,41 +1,70 @@
 import React, { useState } from 'react';
-import { Text, SafeAreaView, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
+import { Text, SafeAreaView, ImageBackground, TextInput, TouchableOpacity, KeyboardAvoidingView, View } from 'react-native';
 import styles from './Index';
 import timerBackground from '../../assets/background/timer.gif'
 
-export default function Home({ navigation }) {
+import axios from 'axios';
 
+export default function Home({ navigation }) {
     const [cidade, setCidade] = useState('');
 
     const [nomeBtn, setNomeBtn] = useState('Iniciar');
 
+    const buscarResultados = () => {
+        const key = '52abfb2c0ec21a123c78b3b918993609';
+        const urlApi = `http://api.openweathermap.org/data/2.5/weather?q=${cidade.toLocaleLowerCase()}&appid=${key}`
+
+        axios.get(urlApi).then(
+            (res) => {
+                const resultados = {
+                    cidade: res.data.name,
+                    velocidadeVento: res.data.wind.speed,
+                    umidade: res.data.main.humidity,
+                    tempAtual: res.data.main.temp,
+                    tempMax: res.data.main.temp_max,
+                    tempMin: res.data.main.temp_min,
+                };
+
+                navigation.navigate('Consulta', resultados, setCidade())
+            }
+        );
+    }
+
     return (
         <SafeAreaView style={styles.container}>
-            <ImageBackground source={timerBackground} style={styles.background}>
+            <ImageBackground resizeMode={'cover'} source={timerBackground} style={styles.background}>
                 <Text style={styles.title}>News</Text>
                 <Text style={styles.titleSub}>Weather</Text>
 
+                <View>
+                    <KeyboardAvoidingView behavior='position'>
+                        {nomeBtn != 'Iniciar' &&
+                            <TextInput
+                                style={styles.inputText}
+                                onChangeText={setCidade}
+                                value={cidade}
+                                placeholder="Digite o nome da sua Cidade/Estado"
+                            />
+                        }
 
-                {nomeBtn != 'Iniciar' &&
-                    <TextInput
-                        style={styles.inputText}
-                        onChangeText={setCidade}
-                        value={cidade}
-                        placeholder="Digite o nome da sua cidade"
-                    />
-                }
+                        {nomeBtn === 'Consultar' &&
+                            <TouchableOpacity style={styles.fakeButton}
+                                onPress={
+                                    () => 
+                                    {buscarResultados()}
+                                }>
+                                <Text style={styles.textButton}>{nomeBtn}</Text>
+                            </TouchableOpacity>
+                        }
 
-                {nomeBtn === 'Consultar' &&
-                    <TouchableOpacity style={styles.fakeButton} onPress={() => navigation.navigate('Consulta')}>
-                        <Text style={styles.textButton}>{nomeBtn}</Text>
-                    </TouchableOpacity>
-                }
+                        {nomeBtn === 'Iniciar' &&
+                            <TouchableOpacity style={styles.fakeButton} onPress={() => setNomeBtn('Consultar')}>
+                                <Text style={styles.textButton}>{nomeBtn}</Text>
+                            </TouchableOpacity>
+                        }
+                    </KeyboardAvoidingView>
+                </View>
 
-                {nomeBtn === 'Iniciar' &&
-                    <TouchableOpacity style={styles.fakeButton} onPress={() => setNomeBtn('Consultar')}>
-                        <Text style={styles.textButton}>{nomeBtn}</Text>
-                    </TouchableOpacity>
-                }
             </ImageBackground>
         </SafeAreaView>
     )
